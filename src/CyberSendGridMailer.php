fde4924e573e52239deea578d731b377158f82f6
Cyberwizard <53139650+cyberwizard-dev@users.noreply.github.com>
2024-02-07 15:11:26 +0100

    Initial commit

---
+++ src/CyberSendGridMailer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Cyberwizard\SendGridMailer;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\View;
+use SendGrid\Mail\TypeException;
+
+class CyberSendGridMailer
+{
+    /**
+     * @throws TypeException
+     * @throws Exception
+     */
+    public static function sendEmail($subject, $to, $templatePath, $data = []): bool
+    {
+        $email = new Mail();
+        $email->setFrom('hello@current.ng', env('APP_NAME'));
+        $email->setSubject($subject);
+        $email->addTo($to);
+
+        $content = View::make($templatePath, $data)->render();
+        $email->addContent('text/html', $content);
+        $sendgrid = new \SendGrid(env('SENDGRID_API_KEY'));
+
+        try {
+            $response = $sendgrid->send($email);
+            Log::info("SendGrid Email Sent - Status Code: {$response->statusCode()}, Headers: " . json_encode($response->headers()) . ", Body: {$response->body()}");
+            return true; // Email sent successfully
+        } catch (Exception $e) {
+            Log::error('Exception caught: ' . $e->getMessage());
+            return false; // Email sending failed
+        }
+    }
+}
